d928b92857ca378f9d499e10f395d375374bbe34
lukazujovic-lz <lukazujovic@gmail.com>
2026-05-27 18:58:24 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,6 @@
+function fun(x) {
+    switch (x) {
+        case 1: var w = window.open("", "", "width=400,height=200,top=350,lefft=800");
+            w.document.write('<p>Igrica je veoma lepa za mladju populaciju,preporucuje se osobama u mladom uzrastu.</p>'); break;
+    }
+}
